e8da9988e99d45dc0f6886af8b3ca58de14065a1
Nessiah919 <nesmith.charlesii@gmail.com>
2021-04-28 23:14:19 -0400

    created a register_contestant method in the Sweepstake class. register_contestant method takes in a contestant object and creates a dictionary based on the Contestant class attributes and then appends each dictionary created to the contestant_list attribute of Sweepstake class.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from contestant import Contestant
-
+from sweepstake import Sweepstake
 
 if __name__ == '__main__':
     def run_simulation():
         contestant_1 = Contestant()
         contestant_2 = Contestant()
 
-        # sweepstake = Sweepstakes()
-        # sweepstake.register_contestant(new_contestant)
+        sweepstake = Sweepstake()
+        sweepstake.register_contestant(contestant_1)
+        sweepstake.register_contestant(contestant_2)
+
+        print(sweepstake.contestant_list)
 
     run_simulation()
--- sweepstake.py
+++ sweepstake.py
@@ -2,16 +2,21 @@ from contestant import Contestant
 
 
 class Sweepstake:
-    def __init__(self, name):
-        self.name = name
-        self.contestants = {}
+    def __init__(self):
+        self.name = input("\nwhat is the name of this sweepstake?: ")
+        self.contestant_list = []
 
     def register_contestant(self, contestant):
+        contestant_dictionary = {"first name": contestant.first_name,
+                                 "last name": contestant.last_name,
+                                 "email": contestant.email_address,
+                                 "registration number": contestant.registration_number}
 
+        self.contestant_list.append(contestant_dictionary)
 
     def pick_winner(self):
-        # returns contestant object
-        pass
+        
 
     def print_contestant_info(self, contestant):
-        pass
+        print(
+            f"contestant info:\n\tfirst name: {contestant.first_name}\nlast name: {contestant.last_name}\nemail: {contestant.email_address}\nregistration number: {contestant.registration_number}")
